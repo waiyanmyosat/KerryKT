39cbbeaab0c8af9bc8b9ed8de687f273094fa900
waiyanmyosat <waiyanmyosat@gmail.com>
2023-07-08 00:29:48 +0700

    first commit

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -9,18 +9,18 @@ export class EmployeeService {
   constructor(private _http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('https://real-blue-pangolin-slip.cyclic.app:8000/employees', data);
+    return this._http.post('https://kerry-backend.vercel.app:3000/employees', data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`https://real-blue-pangolin-slip.cyclic.app:8000/employees/${id}`, data);
+    return this._http.put(`https://kerry-backend.vercel.app:3000/employees/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('https://real-blue-pangolin-slip.cyclic.app:8000/employees');
+    return this._http.get('https://kerry-backend.vercel.app:3000/employees');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`https://real-blue-pangolin-slip.cyclic.app:8000/employees/${id}`);
+    return this._http.delete(`https://kerry-backend.vercel.app:3000/employees/${id}`);
   }
 }
