c77b1b009e76863960d87613113ef316baa9dbb7
waiyanmyosat <138376625+waiyanmyosat@users.noreply.github.com>
2023-07-08 19:23:51 +0700

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -9,18 +9,18 @@ export class EmployeeService {
   constructor(private _http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('https://json-server-in.vercel.app/employees', data);
+    return this._http.post('https://json-server-in.vercel.app/api/posts', data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(https://json-server-in.vercel.app/employees/${id}`, data);
+    return this._http.put(https://json-server-in.vercel.app/api/posts/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('https://json-server-in.vercel.app/employees');
+    return this._http.get('https://json-server-in.vercel.app/api/posts');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`https://json-server-in.vercel.app/employees/${id}`);
+    return this._http.delete(`https://json-server-in.vercel.app/api/posts/${id}`);
   }
 }
